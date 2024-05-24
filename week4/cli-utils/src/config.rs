45421998b884c6a3b2d6ee0956f9e213833281c9
Tamer Alamiri <tameralamiri@gmail.com>
2024-05-23 21:20:12 -0400

    cli example

---
+++ week4/cli-utils/src/config.rs
@@ -0,0 +1,52 @@
+//! This module contains the configuration options for the application.
+//! # Examples:
+//! ```
+//! use cli_utils::config::Logging;
+//! let config = Logging::new();
+//! ```
+//!
+pub enum LogLevel {
+    Debug,
+    Info,
+    Warn,
+    Error,
+}
+
+pub enum LogOutput {
+    Stdout,
+    Stderr,
+    File(String),
+}
+
+/// This struct contains configuration options for the application.
+/// # Examples:
+/// ```
+/// use cli_utils::config::Logging;
+/// let config = Logging::new();
+/// ```
+///
+/// Creating a new instance of the Logging struct:
+/// ```
+/// use cli_utils::config::{Logging, LogLevel, LogOutput};
+/// let config = Logging{ enabled: true, level: LogLevel::Info, destination: LogOutput::Stdout };
+/// ```
+pub struct Logging {
+    pub enabled: bool,
+    pub level: LogLevel,
+    pub destination: LogOutput,
+}
+
+impl Default for Logging {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+impl Logging {
+    pub fn new() -> Self {
+        Self {
+            enabled: false,
+            level: LogLevel::Info,
+            destination: LogOutput::Stdout,
+        }
+    }
+}
---
+++ week4/cli-utils/tests/test_sample.rs
@@ -0,0 +1,5 @@
+#[test]
+fn test_simple() {
+    println!("hello!");
+    //assert!(true);
+}
