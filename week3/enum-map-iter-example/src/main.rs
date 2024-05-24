283cfeed6692e03bf49b20bebcb1cb771341c373
Tamer Alamiri <tameralamiri@gmail.com>
2024-05-23 21:14:43 -0400

    apply linting to main

--- week3/enum-map-iter-example/src/main.rs
+++ week3/enum-map-iter-example/src/main.rs
@@ -1,3 +1,5 @@
+use std::f64::consts::PI;
+
 enum Shape {
     Circle(f64),
     Square(f64),
@@ -5,21 +7,24 @@ enum Shape {
 }
 
 fn main() {
-    let shapes = vec![
+    let shapes = [
         Shape::Circle(3.0),
         Shape::Square(4.0),
         Shape::Triangle(3.0, 4.0),
     ];
-    let total_area: f64 = shapes.iter()
-        .map( |shape| match shape { // map takes a closure with one argument and returns a new iterator
-            Shape::Circle(radius) => 3.14 * radius * radius,
+    let total_area: f64 = shapes
+        .iter()
+        .map(|shape| match shape {
+            // map takes a closure with one argument and returns a new iterator
+            Shape::Circle(radius) => PI * radius * radius,
             Shape::Square(side) => side * side,
             Shape::Triangle(base, height) => 0.5 * base * height,
         })
         .sum();
-    let total_area2: f64 = shapes.iter().fold(0.0, |acc, shape| { // fold takes an initial value and a closure with two arguments: the accumulator and the current value
+    let total_area2: f64 = shapes.iter().fold(0.0, |acc, shape| {
+        // fold takes an initial value and a closure with two arguments: the accumulator and the current value
         match shape {
-            Shape::Circle(radius) => acc + 3.14 * radius * radius, // acc is the accumulator and is updated with the current value
+            Shape::Circle(radius) => acc + PI * radius * radius, // acc is the accumulator and is updated with the current value
             Shape::Square(side) => acc + side * side,
             Shape::Triangle(base, height) => acc + 0.5 * base * height,
         }
